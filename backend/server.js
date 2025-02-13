ae2145dfa25b6c6976aca7a47e08877f7182e132
Duncan Ramuhashi <duncanramohashi@gmail.com>
2025-02-13 14:46:40 +0200

    ww

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 import dotenv from 'dotenv';
 import express from 'express';
-
+import db from './config/mongodb.js'
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-
+db();
 app.listen(PORT,() =>{
     console.log(`Server running on port ${PORT}`);
 });
