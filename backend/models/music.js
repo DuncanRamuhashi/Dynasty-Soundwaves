710717e153ea4902c4d1689f18fc3213f54aad6d
Duncan Ramuhashi <duncanramohashi@gmail.com>
2025-02-18 10:47:45 +0200

    gg

--- backend/models/music.js
+++ backend/models/music.js
@@ -1,1 +1,28 @@
 import mongoose from 'mongoose';
+
+const MusicSchema = new mongoose.Schema({
+    title: { type: String, required: true },  
+    duration: { type: Number, required: true },
+    genre: { type: String, required: true },
+    bpm: { type: Number, required: true },
+    price: { type: Number, required: true },  
+    audio: { type: String, required: true },  
+    sellerID: { type: String, required: true },
+    tags: { type: [String], default: [] },  // Allows up to 5 tags
+    image: { type: String, required: true }
+}, { timestamps: true });
+
+MusicSchema.pre('save', async function(next) {
+    if (this.duration <= 0) {
+        throw new Error('Duration must be a positive number');
+    }
+
+    if (this.bpm <= 0) {
+        throw new Error('BPM must be a positive number');
+    }
+
+    next();
+});
+
+
+export default mongoose.model('Music', MusicSchema);
--- backend/models/terms.js
+++ backend/models/terms.js
@@ -1,1 +1,21 @@
 import mongoose from 'mongoose';
+
+const TermandCondition =  new mongoose.Schema({
+    terms:  { type: String, required: true },
+    conditions:  { type: String, required: true },
+
+},{timestamps:true});
+
+TermandCondition.pre('save', async function (next) {
+    if (this.terms.length < 10) {
+        throw new Error('Terms must be at least 10 characters long.');
+    }
+
+    if (this.conditions.length < 10) {
+        throw new Error('Conditions must be at least 10 characters long.');
+    }
+
+    next();
+});
+
+export default mongoose.model('TermandCondition',TermandCondition);
