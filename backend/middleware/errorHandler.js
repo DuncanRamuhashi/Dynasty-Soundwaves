2bfcd4e386e19f0974cbfc06cc6cbe3cf31b9f18
Duncan Ramuhashi <duncanramohashi@gmail.com>
2025-03-19 15:34:10 +0200

    mk

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -1,10 +1,9 @@
-import { STATUS_CODES } from '../constants/constants.js';
 
-const errorHandler = (err, req, res, next) => {
-    const statusCode = err.statusCode || STATUS_CODES.SERVER_ERROR;
-    res.status(statusCode).json({
-        message: err.message || 'Internal Server Error',
-    });
-};
 
-export default errorHandler;
+class HttpError extends Error {
+    constructor(message,statusCode){
+        super(message);
+        this.statusCode = statusCode;
+    }
+}
+export default HttpError;
